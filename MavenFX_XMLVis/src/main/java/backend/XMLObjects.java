38c3ded653afe7843f39d619bbedc028aabe0acc
20150210 <20150210@IFTS3.htl-stp.if>
2020-01-10 11:51:35 +0100

    added stuff to functions in XMLObjects

--- MavenFX_XMLVis/src/main/java/backend/XMLObjects.java
+++ MavenFX_XMLVis/src/main/java/backend/XMLObjects.java
@@ -7,7 +7,10 @@ package backend;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /**
  *
@@ -20,8 +23,10 @@ public class XMLObjects {
     }
 
     /**
-     * if the panel.xml is not in the Root directory of the programm, please use the readPanel(Path panelPath)
-     * @param panelName 
+     * if the panel.xml is not in the Root directory of the programm, please use
+     * the readPanel(Path panelPath)
+     *
+     * @param panelName
      * @return the root object of the panel.xml (Zentrale)
      */
     public static IXMLObject readPanel(String panelName) {
@@ -29,7 +34,7 @@ public class XMLObjects {
     }
 
     /**
-     * 
+     *
      * @param panelPath
      * @return the root object of the panel.xml (Zentrale)
      */
@@ -38,7 +43,9 @@ public class XMLObjects {
     }
 
     /**
-     * if the facility.xml is not in the Root directory of the programm, please use the readfacility(Path facilityPath)
+     * if the facility.xml is not in the Root directory of the programm, please
+     * use the readfacility(Path facilityPath)
+     *
      * @param facilityName
      * @return all the root objects of the facility.xml
      */
@@ -56,7 +63,9 @@ public class XMLObjects {
     }
 
     /**
-     * if the file is not in the Root directory of the Programm, please use the readUser(Path panelPath)
+     * if the file is not in the Root directory of the Programm, please use the
+     * readUser(Path panelPath)
+     *
      * @param panelName
      * @return all user data found in the given file
      */
@@ -65,7 +74,7 @@ public class XMLObjects {
     }
 
     /**
-     * 
+     *
      * @param panelPath
      * @return all user data found in the given file
      */
@@ -93,5 +102,73 @@ public class XMLObjects {
      */
     public static Collection<LicenceLine> readLicence(Path licenceFile, Path facility) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    /**
+     *
+     * @param path
+     * @return
+     */
+    public static Collection<IXMLObject> readAllFromFile(Path path) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    /**
+     * Creates a tree structure from unstructured IXMLObjects
+     *
+     * @param objects
+     * @return a collection of the root Objects
+     */
+    public static Collection<IXMLObject> createTree(Collection<IXMLObject> objects) {
+        objects.forEach((object) -> {
+            getChildren(objects, object);
+        });
+        return (Collection<IXMLObject>) objects.parallelStream().filter(t -> ((IXMLObject) t).isRoot()).collect(Collectors.toSet());
+    }
+
+    /**
+     * Creates a tree structure from unstructured IXMLObjects
+     *
+     * @param objects
+     * @param ignoreTypes True-> ignores all types given in the types String
+     * array False-> ignores all types except types given types String array
+     * @param types
+     * @return a collection of the root Objects
+     */
+    public static Collection<IXMLObject> createTree(Collection<IXMLObject> objects, boolean ignoreTypes, String... types) {
+        Collection<String> t = Arrays.asList(types);
+        return createTree(objects.parallelStream().filter(a -> ignoreTypes ? !t.contains(a.getType()) : t.contains(a.getType())).collect(Collectors.toSet()));
+    }
+
+    /**
+     * creates a tree structure from a file
+     *
+     * @param path
+     * @return a collection of the root Objects
+     */
+    public static Collection<IXMLObject> createTree(Path path) {
+        return createTree(readAllFromFile(path));
+    }
+
+    /**
+     * Creates a tree structure from file
+     *
+     * @param path
+     * @param ignoreTypes True-> ignores all types given in the types String
+     * array False-> ignores all types except types given types String array
+     * @param types
+     * @return a collection of the root Objects
+     */
+    public static Collection<IXMLObject> createTree(Path path, boolean ignoreTypes, String... types) {
+        return createTree(readAllFromFile(path), ignoreTypes, types);
+    }
+
+    public static Collection<IXMLObject> getChildren(Collection<IXMLObject> objects, IXMLObject parent) {
+        objects.forEach((object) -> {
+            parent.getTags().values().parallelStream().filter((value) -> (object.getUID().toString().equals(value))).forEachOrdered((_item) -> {
+                parent.addChildren(object);
+            });
+        });
+        return parent.getChildren();
     }
 }
