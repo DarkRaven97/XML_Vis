d18ea523d628c03f1d9b275e450f4be63e152601
20150210 <20150210@IFTS3.htl-stp.if>
2020-01-10 11:10:37 +0100

    more read functions

--- MavenFX_XMLVis/src/main/java/backend/XMLObjects.java
+++ MavenFX_XMLVis/src/main/java/backend/XMLObjects.java
@@ -5,6 +5,8 @@
  */
 package backend;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Collection;
 
 /**
@@ -13,15 +15,83 @@ import java.util.Collection;
  */
 public class XMLObjects {
 
-    public static IXMLObject testFile(){
+    public static IXMLObject testFile() {
         return TestObject.getTestRoot();
     }
-    
-    public static IXMLObject readFile(String fileName) {
+
+    /**
+     * if the panel.xml is not in the Root directory of the programm, please use the readPanel(Path panelPath)
+     * @param panelName 
+     * @return the root object of the panel.xml (Zentrale)
+     */
+    public static IXMLObject readPanel(String panelName) {
+        return readPanel(Paths.get(panelName));
+    }
+
+    /**
+     * 
+     * @param panelPath
+     * @return the root object of the panel.xml (Zentrale)
+     */
+    public static IXMLObject readPanel(Path panelPath) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
-    public static Collection<LicenceLine> readLicence(String licenceFile,String p){
+
+    /**
+     * if the facility.xml is not in the Root directory of the programm, please use the readfacility(Path facilityPath)
+     * @param facilityName
+     * @return all the root objects of the facility.xml
+     */
+    public static Collection<IXMLObject> readFacility(String facilityName) {
+        return readFacility(Paths.get(facilityName));
+    }
+
+    /**
+     *
+     * @param facilityPath
+     * @return all root objects of the facility (Villa)
+     */
+    public static Collection<IXMLObject> readFacility(Path facilityPath) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    /**
+     * if the file is not in the Root directory of the Programm, please use the readUser(Path panelPath)
+     * @param panelName
+     * @return all user data found in the given file
+     */
+    public static Collection<IXMLObject> readUser(String panelName) {
+        return readUser(Paths.get(panelName));
+    }
+
+    /**
+     * 
+     * @param panelPath
+     * @return all user data found in the given file
+     */
+    public static Collection<IXMLObject> readUser(Path panelPath) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    /**
+     * if the files are not in the root of the programm you should use the
+     * readLicence using Paths rather than this one
+     *
+     * @param licenceFile Filename of the licence.xml file
+     * @param facility Filename of the facility.xml file
+     * @return all the relevent licence information
+     */
+    public static Collection<LicenceLine> readLicence(String licenceFile, String facility) {
+        return readLicence(Paths.get(licenceFile), Paths.get(facility));
+    }
+
+    /**
+     *
+     * @param licenceFile Path of the licence.xml file
+     * @param facility Path of the facility.xml file
+     * @return all the relevant licence information
+     */
+    public static Collection<LicenceLine> readLicence(Path licenceFile, Path facility) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
