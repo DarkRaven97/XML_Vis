9d2723f0919e2a83eeecc5dd3b0fbb845797ae6c
20150210 <20150210@IFTS3.htl-stp.if>
2020-01-31 10:43:23 +0100

    testing main

---
+++ MavenFX_XMLVis/src/main/java/backend/testing.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package backend;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author 20150210 Klaus Gravogel 3AHIF
+ */
+public class testing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            XMLObjects.readAllFromFile(Paths.get("panel.xml")).forEach(System.out::println);
+        } catch (IOException ex) {
+            Logger.getLogger(testing.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
